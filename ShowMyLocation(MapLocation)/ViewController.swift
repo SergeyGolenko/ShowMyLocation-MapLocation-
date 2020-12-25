11c2bb837ebcb5cd0f676f7da00bbdc6f265d3ed
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-25 12:17:29 +0200

    Initial Commit

---
+++ ShowMyLocation(MapLocation)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowMyLocation(MapLocation)
+//
+//  Created by Сергей Голенко on 25.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
